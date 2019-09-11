3ae34d758a096687500d5516abc528a5750258ad
Glazer, Michael <michael.glazer@cengage.com>
2019-09-11 09:20:59 -0400

    added 3 tests to check for punctuation, case and complete string

--- test.js
+++ test.js
@@ -4,6 +4,14 @@ const inputString = "When not studying nuclear physics, Bambi likes to playbeach
 
 const expectedOutput = "aaaaabbbbcccdeeeeeghhhiiiiklllllllmnnnnooopprsssstttuuvwyyyy";
 
+it("StringSorter function removes punctuation", ()=>{
+    expect(StringSorter(inputString)).toEqual(expectedOutput)
+})
+
+it("StringSorter function maps uppercase to lowercase", ()=>{
+    expect(StringSorter(inputString)).toEqual(expectedOutput)
+})
+
 it("StringSorter function returns sorted string", ()=>{
     expect(StringSorter(inputString)).toEqual(expectedOutput)
 })
